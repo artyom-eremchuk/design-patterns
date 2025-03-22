76629d3c3e8bc58f3f20d8b71f54eade97c93697
Artyom Eremchuk <aeremchuk@rvision.ru>
2025-03-22 12:43:12 +0300

    add singleton

---
+++ src/creational/singleton/singleton.ts
@@ -0,0 +1,32 @@
+class Superhero {
+	private static instance: Superhero;
+	private constructor() {
+		// Private constructor to prevent instantiation
+		console.log("Superhero instance created!");
+	}
+
+	public static getInstance(): Superhero {
+		if (!Superhero.instance) {
+			Superhero.instance = new Superhero();
+		}
+		return Superhero.instance;
+	}
+
+	public performAction(): void {
+		console.log("Superhero is performing an action!");
+	}
+}
+
+// Usage
+function main() {
+	const superman = Superhero.getInstance();
+	const batman = Superhero.getInstance();
+	const wonderwoman = Superhero.getInstance();
+	const flash = Superhero.getInstance();
+
+	console.log(superman === batman); // true
+	console.log(batman === wonderwoman); // true
+	console.log(wonderwoman === flash); // true
+}
+
+main();
